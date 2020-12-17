0a56eab259cd28cbb3e9fc02054bc19d605d0de2
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2020-12-17 16:03:09 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss"
+import {Canvas, useFrame} from "react-three-fiber"
+import React, {useRef, useState} from "react"
+
+import{softShadows,MeshWobbleMaterial, OrbitControls} from "drei"
+import {useSpring, animated} from "react-spring/three";
+softShadows()
+
+const Box = ({position, args, color, speed}) => {
+  const mesh = useRef(null)
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  const [expand, setExpand] = useState(false);
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4]: [1,1,1]
+  })
+  return (
+    <animated.mesh onClick={() => setExpand(!expand)} scale={props.scale} castShadow position={position} ref={mesh}>
+    <boxBufferGeometry attach="geometry" args={args} />
+    <MeshWobbleMaterial attach="material" color={color} speed={speed} factor={0.8} />
+    </animated.mesh>
+  )
+}
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    <Canvas shadowMap colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
+      <ambientLight intensity={0.3} />
+      <directionalLight
+      castShadow
+      position={[0,10,0]}
+      intensity={1.5}
+      shadow-mapSize-width={1024}
+      shadow-mapSize-height={1024}
+      shadow-camera-far={50}
+      shadow-camera-left={-10}
+      shadow-camera-right={10}
+      shadow-camera-top={10}
+      shadow-camera-bottom={-10}
+      />
+      <pointLight position={[-10, 0, -20]} intensity={0.5} />
+      <pointLight position={[0, -20, 0]} intensity={0.9} />
+   
+   <group>
+     <mesh receiveShadow rotation={[-Math.PI / 2, 0 , 0]} position={[0,-3,0]}>
+       <planeBufferGeometry attach="geometry" args={[100,100]} />
+       <shadowMaterial attach="material" opacity={0.4} />
+     </mesh>
+      
+   <Box position={[0,1,0]} args={[3,2,1]} color={"lightblue"} speed={1} />
+   <Box position={[-2,1,-5]} color="pink" speed={6} />
+   <Box position={[5,1,-2]} color="pink" speed={7} />
+   </group>
+  
+   <OrbitControls />
+    </Canvas>
+    </>
   );
 }
 
